959941f5414a7cd3a601c340d2a799fb20a6fbac
Bastien Dalla Piazza <Bastien Dalla Piazza>
2014-06-30 17:38:17 +0200

    Actually changed the sign this time

--- LogJastrowPotential.h
+++ LogJastrowPotential.h
@@ -30,7 +30,7 @@ class LogJastrowPotential: public JastrowPotential {
                 int ph=1;
                 if(linalg::mod(rx+ry,2))
                     ph=-1;
-                return ph*m_params[0]*log(sqrt(0.5*(pow(sin(rx*M_PI/m_lattice->GetLx()),2)+pow(sin(ry*M_PI/m_lattice->GetLy()),2))))/m_lattice->GetNv();
+                return -ph*m_params[0]*log(sqrt(0.5*(pow(sin(rx*M_PI/m_lattice->GetLx()),2)+pow(sin(ry*M_PI/m_lattice->GetLy()),2))))/m_lattice->GetNv();
             }
         }
         virtual double space_potential_grad(const uint_vec_t& Ri,
