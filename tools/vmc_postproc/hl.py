44bae2fd57c0c5862279fa1939e1c915c47e35f9
Bastien Dalla Piazza <Bastien Dalla Piazza>
2014-08-18 11:28:41 +0200

    fixed postprocessing bugs. Changed arguments terminology

--- tools/vmc_postproc/hl.py
+++ tools/vmc_postproc/hl.py
@@ -1,6 +1,6 @@
 #!/bin/env python
 
-from vmc_postproc import proc,load
+from vmc_postproc import proc,load,stagflux,sfpnxphz
 import numpy as np
 import re
 import six
@@ -16,7 +16,7 @@ def get_eig_sys(filenames,nsamp,wav=None,tol=1e-12):
         mo=re.search(r'(.*/[0-9]+)-ProjHeis\.h5',filenames[0])
         wav=mo.groups()[0]+'-WaveFunction.h5'
     wav_st=load.get_wav(wav)
-    fs=np.diag(proc.fermisigns(wav_st))
+    fs=np.diag(proc.fermisigns(wav_st,load.get_attr(filenames[0])))
     H=np.einsum('ij,kjl,lm->kim',fs,H,fs)
     O=np.einsum('ij,kjl,lm->kim',fs,O,fs)
     evals=np.zeros(H.shape[:2])
@@ -24,3 +24,31 @@ def get_eig_sys(filenames,nsamp,wav=None,tol=1e-12):
     for s in range(nsamp):
         evals[s,:],evecs[s,:,:]=proc.geneigh(H[s,:,:],O[s,:,:],tol)
     return H,O,evals,evecs
+
+def get_sq_ampl(filenames,nsamp,wav=None,tol=1e-12,H=None,O=None,evals=None,evecs=None):
+    if type(filenames)!=list:
+        filenames=[filenames]
+    if H==None:
+        H,O,evals,evecs=get_eig_sys(filenames,nsamp,wav,tol)
+    qp_mod=None
+    params=load.get_attr(filenames[0])
+    if params['sfpnxphz_wav']:
+        qp_mod=sfpnxphz
+    elif params['stagflux_wav']:
+        qp_mod=stagflux
+    else:
+        raise RuntimeError('sfpnzphx_wav not implemented yet')
+    pkq=qp_mod.spinops(params)
+    if params['stagflux_wav']:
+        if params['channel']=='trans':
+            return abs(np.einsum('i,sij,sjk->sk',np.conj(pkq[1]),O,evecs))**2
+        elif params['channel']=='long':
+            return abs(np.einsum('i,sij,sjk->sk',np.conj(pkq[2]),O,evecs))**2
+        else:
+            raise RuntimeError('channel not understood.')
+    elif params['sfpnxphz_wav']:
+        out=[None]*3
+        for a in range(3):
+            out[a]=abs(np.einsum('i,sij,sjk->sk',np.conj(pkq[a]),O,evecs))**2
+        return out
+
--- tools/vmc_postproc/sfpnxphz.py
+++ tools/vmc_postproc/sfpnxphz.py
@@ -4,15 +4,33 @@ import numpy as np
 from vmc_postproc import recspace
 
 def delta(kx,ky,params):
-    return 0.5*(np.exp(1j*params['phi']*np.pi)*np.cos(kx*2*np.pi)+\
-                np.exp(-1j*params['phi']*np.pi)*np.cos(ky*2*np.pi))
+    return 0.5*(np.exp(1j*params['phi'])*np.cos(kx*2*np.pi)+\
+                np.exp(-1j*params['phi'])*np.cos(ky*2*np.pi))
+
+def mfham(kx,ky,params):
+    H=np.zeros([4,4]+list(np.shape(kx)),dtype=complex)
+    dk=delta(kx,ky,params)
+    H[0,0,:]=params['field']
+    H[0,1,:]=-dk.conjugate()
+    H[0,2,:]=params['neel']
+    H[1,0,:]=-dk
+    H[1,1,:]=params['field']
+    H[1,3,:]=-params['neel']
+    H[2,0,:]=params['neel']
+    H[2,2,:]=-params['field']
+    H[2,3,:]=-dk.conjugate()
+    H[3,1,:]=-params['neel']
+    H[3,2,:]=-dk
+    H[3,3,:]=-params['field']
+    Tp=trans(kx,ky,params)
+    return np.einsum('ij...,jk...,lk...->il...',Tp,H,Tp)
 
 def evals(kx,ky,params):
     v=np.zeros([4]+list(np.shape(kx)))
-    v[0,:]=-np.sqrt(params['nx']**2+(params['hz']-abs(delta(kx,ky,params)))**2)
-    v[1,:]=-np.sqrt(params['nx']**2+(params['hz']+abs(delta(kx,ky,params)))**2)
-    v[2,:]=np.sqrt(params['nx']**2+(params['hz']-abs(delta(kx,ky,params)))**2)
-    v[3,:]=np.sqrt(params['nx']**2+(params['hz']+abs(delta(kx,ky,params)))**2)
+    v[0,:]=-np.sqrt(params['neel']**2+(params['field']-abs(delta(kx,ky,params)))**2)
+    v[1,:]=-np.sqrt(params['neel']**2+(params['field']+abs(delta(kx,ky,params)))**2)
+    v[2,:]=np.sqrt(params['neel']**2+(params['field']-abs(delta(kx,ky,params)))**2)
+    v[3,:]=np.sqrt(params['neel']**2+(params['field']+abs(delta(kx,ky,params)))**2)
     return v
 
 def evecs(kx,ky,params):
@@ -20,27 +38,39 @@ def evecs(kx,ky,params):
     ev=evals(kx,ky,params)
     dk=delta(kx,ky,params)
     # band 1
-    v[0,0,:]=0.5*np.sqrt(1.0-params['nx']/evals(kx,ky,params)[0,:])
-    v[1,0,:]=0.5*np.sign(abs(dk)-params['hz'])*np.exp(1j*np.angle(dk))*np.sqrt(1.0+params['nx']/evals(kx,ky,params)[0,:])
-    v[2,0,:]=0.5*np.sign(abs(dk)-params['hz'])*np.sqrt(1.0+params['nx']/evals(kx,ky,params)[0,:])
-    v[3,0,:]=0.5*np.exp(1j*np.angle(dk))*np.sqrt(1.0-params['nx']/evals(kx,ky,params)[0,:])
+    v[0,0,:]=0.5*np.sqrt(1.0-params['neel']/evals(kx,ky,params)[0,:])
+    v[1,0,:]=0.5*np.sign(abs(dk)-params['field'])*np.exp(1j*np.angle(dk))*np.sqrt(1.0+params['neel']/evals(kx,ky,params)[0,:])
+    v[2,0,:]=0.5*np.sign(abs(dk)-params['field'])*np.sqrt(1.0+params['neel']/evals(kx,ky,params)[0,:])
+    v[3,0,:]=0.5*np.exp(1j*np.angle(dk))*np.sqrt(1.0-params['neel']/evals(kx,ky,params)[0,:])
     # band 2
-    v[0,1,:]=0.5*np.sqrt(1.0-params['nx']/evals(kx,ky,params)[1,:])
-    v[1,1,:]=0.5*np.sign(abs(dk)+params['hz'])*np.exp(1j*np.angle(dk))*np.sqrt(1.0+params['nx']/evals(kx,ky,params)[1,:])
-    v[2,1,:]=-0.5*np.sign(abs(dk)+params['hz'])*np.sqrt(1.0+params['nx']/evals(kx,ky,params)[1,:])
-    v[3,1,:]=-0.5*np.exp(1j*np.angle(dk))*np.sqrt(1.0-params['nx']/evals(kx,ky,params)[1,:])
+    v[0,1,:]=0.5*np.sqrt(1.0-params['neel']/evals(kx,ky,params)[1,:])
+    v[1,1,:]=0.5*np.sign(abs(dk)+params['field'])*np.exp(1j*np.angle(dk))*np.sqrt(1.0+params['neel']/evals(kx,ky,params)[1,:])
+    v[2,1,:]=-0.5*np.sign(abs(dk)+params['field'])*np.sqrt(1.0+params['neel']/evals(kx,ky,params)[1,:])
+    v[3,1,:]=-0.5*np.exp(1j*np.angle(dk))*np.sqrt(1.0-params['neel']/evals(kx,ky,params)[1,:])
     # band 3
-    v[0,2,:]=0.5*np.sqrt(1.0-params['nx']/evals(kx,ky,params)[2,:])
-    v[1,2,:]=-0.5*np.sign(abs(dk)-params['hz'])*np.exp(1j*np.angle(dk))*np.sqrt(1.0+params['nx']/evals(kx,ky,params)[2,:])
-    v[2,2,:]=-0.5*np.sign(abs(dk)-params['hz'])*np.sqrt(1.0+params['nx']/evals(kx,ky,params)[2,:])
-    v[3,2,:]=0.5*np.exp(1j*np.angle(dk))*np.sqrt(1.0-params['nx']/evals(kx,ky,params)[2,:])
+    v[0,2,:]=0.5*np.sqrt(1.0-params['neel']/evals(kx,ky,params)[2,:])
+    v[1,2,:]=-0.5*np.sign(abs(dk)-params['field'])*np.exp(1j*np.angle(dk))*np.sqrt(1.0+params['neel']/evals(kx,ky,params)[2,:])
+    v[2,2,:]=-0.5*np.sign(abs(dk)-params['field'])*np.sqrt(1.0+params['neel']/evals(kx,ky,params)[2,:])
+    v[3,2,:]=0.5*np.exp(1j*np.angle(dk))*np.sqrt(1.0-params['neel']/evals(kx,ky,params)[2,:])
     # band 4
-    v[0,3,:]=0.5*np.sqrt(1.0-params['nx']/evals(kx,ky,params)[3,:])
-    v[1,3,:]=-0.5*np.sign(params['hz']+abs(dk))*np.exp(1j*np.angle(dk))*np.sqrt(1.0+params['nx']/evals(kx,ky,params)[3,:])
-    v[2,3,:]=0.5*np.sign(params['hz']+abs(dk))*np.sqrt(1.0+params['nx']/evals(kx,ky,params)[3,:])
-    v[3,3,:]=-0.5*np.exp(1j*np.angle(dk))*np.sqrt(1.0-params['nx']/evals(kx,ky,params)[3,:])
+    v[0,3,:]=0.5*np.sqrt(1.0-params['neel']/evals(kx,ky,params)[3,:])
+    v[1,3,:]=-0.5*np.sign(params['field']+abs(dk))*np.exp(1j*np.angle(dk))*np.sqrt(1.0+params['neel']/evals(kx,ky,params)[3,:])
+    v[2,3,:]=0.5*np.sign(params['field']+abs(dk))*np.sqrt(1.0+params['neel']/evals(kx,ky,params)[3,:])
+    v[3,3,:]=-0.5*np.exp(1j*np.angle(dk))*np.sqrt(1.0-params['neel']/evals(kx,ky,params)[3,:])
     return v
     #return np.einsum('ij...->ji...',v).conjugate()
+
+def trans(kx,ky,params):
+    Tp=np.zeros([4,4]+list(np.shape(kx)))
+    Tp[0,0,:]=1
+    Tp[0,2,:]=-1
+    Tp[1,1,:]=1
+    Tp[1,3,:]=-1
+    Tp[2,0,:]=1
+    Tp[2,2,:]=1
+    Tp[3,1,:]=1
+    Tp[3,3,:]=1
+    return Tp/np.sqrt(2)
 
 def spinops(params):
     kx,ky=recspace.mbz(params)
@@ -53,37 +83,43 @@ def spinops(params):
     if (params['qx'],params['qy']) in [(0,0),(params['L']/2,params['L']/2)]:
         nexc+=1
         s=1
-    so=np.zeros((nexc,3),dtype=complex)
+    so=[np.zeros((nexc),dtype=complex)]*3
     #Sqx
-    so[s::4,0]=0.5*(-evk[0,2,:].conjugate()*evq[0,0,:]+evk[2,2,:].conjugate()*evq[2,0,:]+\
-               skq*(-evk[1,2,:].conjugate()*evq[1,0,:]+evk[3,2,:].conjugate()*evq[3,0,:]))
-    so[s+1::4,0]=0.5*(-evk[0,3,:].conjugate()*evq[0,0,:]+evk[2,3,:].conjugate()*evq[2,0,:]+\
+    so[0][s::4]=0.5*(-evk[0,2,:].conjugate()*evq[0,0,:]+evk[2,2,:].conjugate()*evq[2,0,:]+\
+                 skq*(-evk[1,2,:].conjugate()*evq[1,0,:]+evk[3,2,:].conjugate()*evq[3,0,:]))
+    so[0][s+1::4]=0.5*(-evk[0,3,:].conjugate()*evq[0,0,:]+evk[2,3,:].conjugate()*evq[2,0,:]+\
                  skq*(-evk[1,3,:].conjugate()*evq[1,0,:]+evk[3,3,:].conjugate()*evq[3,0,:]))
-    so[s+2::4,0]=0.5*(-evk[0,2,:].conjugate()*evq[0,1,:]+evk[2,2,:].conjugate()*evq[2,1,:]+\
+    so[0][s+2::4]=0.5*(-evk[0,2,:].conjugate()*evq[0,1,:]+evk[2,2,:].conjugate()*evq[2,1,:]+\
                  skq*(-evk[1,2,:].conjugate()*evq[1,1,:]+evk[3,2,:].conjugate()*evq[3,1,:]))
-    so[s+3::4,0]=0.5*(-evk[0,3,:].conjugate()*evq[0,1,:]+evk[2,3,:].conjugate()*evq[2,1,:]+\
+    so[0][s+3::4]=0.5*(-evk[0,3,:].conjugate()*evq[0,1,:]+evk[2,3,:].conjugate()*evq[2,1,:]+\
                  skq*(-evk[1,3,:].conjugate()*evq[1,1,:]+evk[3,3,:].conjugate()*evq[3,1,:]))
     #Sqy
-    so[s::4,1]=0.5j*(evk[0,2,:].conjugate()*evq[2,0,:]-evk[2,2,:].conjugate()*evq[0,0,:]+\
+    so[1][s::4]=0.5j*(evk[0,2,:].conjugate()*evq[2,0,:]-evk[2,2,:].conjugate()*evq[0,0,:]+\
                 skq*(evk[1,2,:].conjugate()*evq[3,0,:]-evk[3,2,:].conjugate()*evq[1,0,:]))
-    so[s+1::4,1]=0.5j*(evk[0,3,:].conjugate()*evq[2,0,:]-evk[2,3,:].conjugate()*evq[0,0,:]+\
+    so[1][s+1::4]=0.5j*(evk[0,3,:].conjugate()*evq[2,0,:]-evk[2,3,:].conjugate()*evq[0,0,:]+\
                   skq*(evk[1,3,:].conjugate()*evq[3,0,:]-evk[3,3,:].conjugate()*evq[1,0,:]))
-    so[s+2::4,1]=0.5j*(evk[0,2,:].conjugate()*evq[2,1,:]-evk[2,2,:].conjugate()*evq[0,1,:]+\
+    so[1][s+2::4]=0.5j*(evk[0,2,:].conjugate()*evq[2,1,:]-evk[2,2,:].conjugate()*evq[0,1,:]+\
                   skq*(evk[1,2,:].conjugate()*evq[3,1,:]-evk[3,2,:].conjugate()*evq[1,1,:]))
-    so[s+3::4,1]=0.5j*(evk[0,3,:].conjugate()*evq[2,1,:]-evk[2,3,:].conjugate()*evq[0,1,:]+\
+    so[1][s+3::4]=0.5j*(evk[0,3,:].conjugate()*evq[2,1,:]-evk[2,3,:].conjugate()*evq[0,1,:]+\
                   skq*(evk[1,3,:].conjugate()*evq[3,1,:]-evk[3,3,:].conjugate()*evq[1,1,:]))
     #Sqz
-    so[s::4,2]=0.5*(evk[0,2,:].conjugate()*evq[2,0,:]+evk[2,2,:].conjugate()*evq[0,0,:]+\
+    so[2][s::4]=0.5*(evk[0,2,:].conjugate()*evq[2,0,:]+evk[2,2,:].conjugate()*evq[0,0,:]+\
                skq*(evk[1,2,:].conjugate()*evq[3,0,:]+evk[3,2,:].conjugate()*evq[1,0,:]))
-    so[s+1::4,2]=0.5*(evk[0,3,:].conjugate()*evq[2,0,:]+evk[2,3,:].conjugate()*evq[0,0,:]+\
+    so[2][s+1::4]=0.5*(evk[0,3,:].conjugate()*evq[2,0,:]+evk[2,3,:].conjugate()*evq[0,0,:]+\
                  skq*(evk[1,3,:].conjugate()*evq[3,0,:]+evk[3,3,:].conjugate()*evq[1,0,:]))
-    so[s+2::4,2]=0.5*(evk[0,2,:].conjugate()*evq[2,1,:]+evk[2,2,:].conjugate()*evq[0,1,:]+\
+    so[2][s+2::4]=0.5*(evk[0,2,:].conjugate()*evq[2,1,:]+evk[2,2,:].conjugate()*evq[0,1,:]+\
                  skq*(evk[1,2,:].conjugate()*evq[3,1,:]+evk[3,2,:].conjugate()*evq[1,1,:]))
-    so[s+3::4,2]=0.5*(evk[0,3,:].conjugate()*evq[2,1,:]+evk[2,3,:].conjugate()*evq[0,1,:]+\
+    so[2][s+3::4]=0.5*(evk[0,3,:].conjugate()*evq[2,1,:]+evk[2,3,:].conjugate()*evq[0,1,:]+\
                  skq*(evk[1,3,:].conjugate()*evq[3,1,:]+evk[3,3,:].conjugate()*evq[1,1,:]))
     if (params['qx'],params['qy']) in [(0,0),(params['L']/2,params['L']/2)]:
-        so[0,0]=0.5*np.sum(-evk[0,0,:].conjugate()*evq[0,0,:]+evk[2,0,:].conjugate()*evq[2,0,:]+\
+        so[0][0]=0.5*np.sum(-evk[0,0,:].conjugate()*evq[0,0,:]+evk[2,0,:].conjugate()*evq[2,0,:]+\
                     skq*(-evk[1,0,:].conjugate()*evq[1,0,:]+evk[3,0,:].conjugate()*evq[3,0,:]))+\
-                0.5*np.sum(-evk[0,1,:].conjugate()*evq[0,1,:]+evk[2,1,:].conjugate()*evq[2,1,:]+\
+                 0.5*np.sum(-evk[0,1,:].conjugate()*evq[0,1,:]+evk[2,1,:].conjugate()*evq[2,1,:]+\
                     skq*(-evk[1,1,:].conjugate()*evq[1,1,:]+evk[3,1,:].conjugate()*evq[3,1,:]))
     return so
+
+def refstate(params):
+    state=np.zeros(2*params['L']**2)
+    state[::4]=1
+    state[1::4]=1
+    return state
